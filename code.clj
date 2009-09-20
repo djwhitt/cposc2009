0c3a53b488a2e8f58037defbd00541a5c8b8ff16
David Whittington <djwhitt@gmail.com>
2009-09-19 21:57:02 -0400

    Adding some basic syntax examples

--- code.clj
+++ code.clj
@@ -1,3 +1,81 @@
+;{{{ REPL
+
+; Clojure has a REPL just like Python, Ruby, Erlang ... etc.
+;
+; One useful feature for this presentation: *1 *2 *3 refer can be used
+; to refer to previous return values.
+
+;}}}
+
+;{{{ Atoms
+
+"hi"   ; string
+
+123    ; number
+
+:key   ; keyword
+
+5/72   ; ratio
+
+4.2    ; float
+
+nil    ; nil
+
+true   ; true
+
+false  ; false
+
+;}}}
+
+;{{{ Collections
+
+'(:key 123 "string")  ; list
+
+[:key 123 "string"]   ; vector
+
+{:width 2, :height 4} ; map
+
+#{0 1 2 3}            ; set
+
+;}}}
+
+;{{{ Collections 2
+
+; vectors, maps, and sets are callable
+
+([\a \b \c] 0)
+
+([\a \b \c] 1)
+
+;}}}
+
+;{{{ Sequences
+
+;}}}
+
+;{{{ Truthiness
+
+; is there a builtin function for this?
+(defn truthy? [x]
+  (if x
+    true
+    false))
+
+(truthy? true)
+
+(truthy? 1)
+
+(truthy? 0)
+
+(truthy [1 2 3])
+
+(truthy? nil)
+
+(truthy? false)
+
+;}}}
+
+
 (ns com.djwhitt.cljchat
   (:use [clojure.contrib.pprint])
   (:use [compojure]))
@@ -42,3 +120,5 @@
     "/*" (servlet cljchat)))
 
 (clojure.main/repl)
+
+; vim: set foldmethod=marker:
