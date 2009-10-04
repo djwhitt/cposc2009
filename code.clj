d55fc80d483c3e17ec0cb29867cbd9c59cb9b8b9
David Whittington <djwhitt@gmail.com>
2009-10-03 20:36:40 -0400

    More code and notes

--- code.clj
+++ code.clj
@@ -1,4 +1,4 @@
-;{{{ REPL
+;{{{ 1 - REPL
 
 ; Clojure has a REPL just like Python, Ruby, Erlang ... etc.
 ;
@@ -7,7 +7,7 @@
 
 ;}}}
 
-;{{{ Atoms
+;{{{ 2 - Atoms
 
 "a string" ; string
 
@@ -29,7 +29,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Collections
+;{{{ 3 - Collections
 
 '(:a :b :c)  ; list
 
@@ -41,7 +41,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Expressions
+;{{{ 4 - Expressions
 
 ; Everything is an expression. There are no statements.
 ; 
@@ -57,7 +57,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Truthiness
+;{{{ 5 - Truthiness
 
 ; false and nil are considered false in an if expression
 ; everything else is considered true (including 0)
@@ -82,41 +82,63 @@ false      ; false
 
 ;}}}
  
-;{{{ Collections 2
+;{{{ 6 - Collections 2
+
+; get is used to access items in indexed collections
+  
+(get [:a :b :c] 0)
+
+(get {:a 1, :b 2} :a)
+
+(get #{:a :b :c} :a)
 
 ; vectors, maps, and sets are callable
-
+ 
 ([:a :b :c] 0)
 
 ([:a :b :c] 1)
 
 ({:a 1, :b 2} :a)
 
-(:a {:a 1, :b 2})
-
 (#{:a :b :c} :a)
 
 (#{:a :b :c} :d)
 
-;}}}
-
-;{{{ Sequences
-
-;}}}
-
-;{{{ Functions
+; keywords are also callable - they look themselves up in maps
+ 
+(:a {:a 1, :b 2})
 
 ;}}}
 
-;{{{ Vars
+;{{{ 7 - Sequences
+
+;}}}
+
+;{{{ 8 - Functions
+
+;}}}
+
+;{{{ 9 - Vars
 
 ;}}}
   
-;{{{ Refs and Transactions
+;{{{ 10 - Refs and Transactions
+
+(def messages (ref ["First post" "blah blah blah" "... comparison to Hitler ..."]))
+
+messages
+
+@messages
+
+(dosync (ref-set messages (conj @messages "flame flame flame")))
+
+(dosync (alter messages conj "flame flame flame"))
+
+(dosync (commute messages conj "flame flame flame"))
 
 ;}}}
 
-;{{{ Compojure Demo Part 1 - Hello world
+;{{{ 11 - Compojure Demo Part 1 - Hello world
 
 (ns com.djwhitt.cljchat
   (:use [clojure.contrib.pprint])
@@ -131,7 +153,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Compojure Demo Part 2 - Adding a view function
+;{{{ 12 - Compojure Demo Part 2 - Adding a view function
 
 (defn view []
   (html [:h1 "Hello CPOSC 2009"]))
@@ -141,7 +163,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Compojure Demo Part 3 - Rendering messages
+;{{{ 13 - Compojure Demo Part 3 - Rendering messages
 
 (def messages (ref ["test message 1" "test message 2"]))
 
@@ -156,7 +178,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Compojure Demo Part 4 - Adding a form
+;{{{ 14 - Compojure Demo Part 4 - Adding a form
 
 (defn render-message-form []
   (form-to [:post "/"]
@@ -170,7 +192,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Compojure Demo Part 5 - Adding a post action
+;{{{ 15 - Compojure Demo Part 5 - Adding a post action
 
 (defn post-message [msgs msg]
   (do
@@ -183,7 +205,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Compojure Demo Part 6 - Adding a layout
+;{{{ 16 - Compojure Demo Part 6 - Adding a layout
  
 (defn layout [& body]
   (html
@@ -202,7 +224,7 @@ false      ; false
 
 ;}}}
 
-;{{{ Compojure Demo Part 7 - Adding some ajax
+;{{{ 17 - Compojure Demo Part 7 - Adding some ajax
 
 (defroutes cljchat
   (GET  "/*" (or (serve-file (params :*)) :next))
